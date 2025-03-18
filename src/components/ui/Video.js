4825887133b4101302e683f6925b1dc29b25b8f2
zs-wenxing <157490456+zs-wenxing@users.noreply.github.com>
2025-03-18 14:06:32 +0800

    Ensure Video is auto play

--- src/components/ui/Video.js
+++ src/components/ui/Video.js
@@ -6,10 +6,12 @@ export default function Video({filePath, fileType, containerStyle, videoStyle}) 
     <div className={`${containerStyle}`}>
 
       <video
-          width="300"
-          className={videoStyle}
-          autoPlay
-          loop
+        width="300"
+        className={videoStyle}
+        autoPlay
+        loop
+        muted 
+        playsInline
       >
           <source src={filePath} type={fileType} />
           Your browser does not support the video tag.
