ad88fe99bdd6ee9693d4a23113c6a8c8ceee4f26
zs-wenxing <157490456+zs-wenxing@users.noreply.github.com>
2025-03-19 00:02:29 +0800

    Responsive for Easy Analysis Section

--- src/components/sections/EasyAnalysisSection.js
+++ src/components/sections/EasyAnalysisSection.js
@@ -1,86 +1,71 @@
 "use client"; // Runs on the client side
 
+import styles from "@/styles/section/EasyAnalysisSection.module.scss";
 import Image from "next/image";
 
 export default function EasyAnalysisSection() {
   return (
-    <section>
-      <div className="w-full mt-[60px]">
-        
-        {/* title */}
-        <h1 className="text-center text-4xl font-semibold" style={{color: 'var(--light-blue)'}}>Easy Analysis</h1>
+    <section className={styles.easyAnalysisContainer}>
+      {/* Title */}
+      <h1 className={styles.title}>Easy Analysis</h1>
 
-        {/* Description */}
-        <p className="text-center font-semibold text-xl max-w-[850px] mx-auto mt-[28px]" style={{color: 'var(--gold)'}}>
-            Instant clarity on the Masters' profile. Get a snapshot of their trade history, profitability, risk, and portfolio all in one place. 
-        </p>
+      {/* Description */}
+      <p className={styles.description}>
+        Instant clarity on the Masters' profile. Get a snapshot of their trade history, profitability, risk, and portfolio all in one place.
+      </p>
 
-        {/* Mobile & Function Images */}
-        <div className="flex justify-center mt-[36px] relative">
+      {/* Mobile & Function Images */}
+      <div className={styles.imageContainer}>
 
-            <div className="relative">
-            
-                <Image 
-                    src="/images/mobile-2.png" 
-                    width={200} 
-                    height={500} 
-                    alt="mobile-1"
-                    style={{ width: "196px" }}
-                    className="shadow-[0px_0px_36px_rgba(0,0,0,0.5)] rounded-[24px] z-0"
-                />
+        <div className="relative">
+          <Image 
+            src="/images/mobile-2.png" 
+            width={200} 
+            height={500} 
+            alt="mobile-1"
+            className={styles.mobileImage}
+          />
 
-                {/* function 1 */}
-                <Image 
-                    src="/images/analysis/function-1.png" 
-                    width={200} 
-                    height={500} 
-                    alt="function-1"
-                    style={{ width: "196px" }}
-                    className="z-10 absolute top-[-22px] left-[-98px]"
-                />
-                
-                {/* function 2 */}
-                <Image 
-                    src="/images/analysis/function-2.png" 
-                    width={200} 
-                    height={500} 
-                    alt="function-2"
-                    style={{ width: "196px" }}
-                    className="z-10 absolute top-[68px] right-[-98px]"
-                />
+          {/* Function Images */}
+          <Image 
+            src="/images/analysis/function-1.png" 
+            width={200} 
+            height={500} 
+            alt="function-1"
+            className={`${styles.functionImage} ${styles.function1}`}
+          />
 
-                {/* function 3 */}
-                <Image 
-                    src="/images/analysis/function-3.png" 
-                    width={200} 
-                    height={500} 
-                    alt="function-3"
-                    style={{ width: "196px" }}
-                    className="z-10 absolute top-[138px] left-[-98px]"
-                />
+          <Image 
+            src="/images/analysis/function-2.png" 
+            width={200} 
+            height={500} 
+            alt="function-2"
+            className={`${styles.functionImage} ${styles.function2}`}
+          />
 
-                {/* function 4 */}
-                <Image 
-                    src="/images/analysis/function-4.png" 
-                    width={200} 
-                    height={500} 
-                    alt="function-4"
-                    style={{ width: "196px" }}
-                    className="z-10 absolute top-[188px] right-[-98px]"
-                />
+          <Image 
+            src="/images/analysis/function-3.png" 
+            width={200} 
+            height={500} 
+            alt="function-3"
+            className={`${styles.functionImage} ${styles.function3}`}
+          />
 
-                {/* function 5 */}
-                <Image 
-                    src="/images/analysis/function-5.png" 
-                    width={200} 
-                    height={500} 
-                    alt="function-5"
-                    style={{ width: "196px" }}
-                    className="z-10 absolute bottom-[-22px] left-[-98px]"
-                />
+          <Image 
+            src="/images/analysis/function-4.png" 
+            width={200} 
+            height={500} 
+            alt="function-4"
+            className={`${styles.functionImage} ${styles.function4}`}
+          />
 
-            </div>
-
+          <Image 
+            src="/images/analysis/function-5.png" 
+            width={200} 
+            height={500} 
+            alt="function-5"
+            className={`${styles.functionImage} ${styles.function5}`}
+          />
         </div>
 
       </div>
